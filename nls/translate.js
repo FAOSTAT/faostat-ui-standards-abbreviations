b169e9ff1f676a8bc286a4109ea6563cda972a88
Kalimaha <guido.barbaglia@gmail.com>
2015-09-21 16:03:19 +0200

    Abbreviations module.

--- nls/translate.js
+++ nls/translate.js
@@ -1,6 +1,10 @@
+/*global define*/
 define({
     root : {
-        error: 'FAOSTAT Standards Abbreviations Error'
+        error: 'FAOSTAT Standards Abbreviations Error',
+        acronym_label: 'Acronym',
+        definition_label: 'Definition',
+        faostat_abbreviations_label: 'FAOSTAT Abbreviations'
     },
     fr : true,
     es : true
